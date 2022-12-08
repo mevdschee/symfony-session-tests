297d0eab7322a4a06b9a3f79a974214450bb707f
Maurits van der Schee <maurits@vdschee.nl>
2022-12-08 14:03:01 +0100

    update

---
+++ run-tests.php
@@ -0,0 +1,154 @@
+<?php
+chdir(__DIR__);
+require "vendor/autoload.php";
+$handlers = ['memcached', 'redis'];
+$extensions = ['memcached', 'redis'];
+$parallel = 10;
+// execute single test
+if ($_SERVER['SERVER_PORT'] ?? 0) {
+
+    header('Content-Type: text/plain');
+
+    $path = trim($_SERVER['QUERY_STRING'], '/');
+    list($handlerName, $fileName) = explode('/', $path, 2);
+
+    switch ($handlerName) {
+        case 'redis':
+            $redis = new Redis();
+            $redis->connect('localhost', '6379');
+            $handler = new \Symfony\Component\HttpFoundation\Session\Storage\Handler\RedisSessionHandler($redis);
+            break;
+        case 'memcached':
+            $memcached = new Memcached();;
+            $memcached->addServer('localhost', 11211);
+            $handler = new \Symfony\Component\HttpFoundation\Session\Storage\Handler\MemcachedSessionHandler($memcached);
+            break;
+        default:
+            die('invalid handler name');
+    }
+
+    include 'src/LoggingSessionHandler.php';
+    session_set_save_handler(new MintyPHP\LoggingSessionHandler($handler), true);
+    header('X-Session-Save-Path: ' . ini_get('session.save_path'));
+
+    ob_start();
+
+    if (!preg_match('/^[a-z0-9_-]+$/', $fileName)) {
+        die('invalid file name');
+    }
+    if (file_exists("tests/src/$fileName.php")) {
+        include "tests/src/$fileName.php";
+    }
+
+    // get timestamp and content
+    list($msec, $sec) = explode(' ', microtime());
+    $timestamp = $sec . substr($msec, 2, 6);
+    header("X-Session-Flush-At: $timestamp");
+    ob_end_flush();
+
+    die();
+}
+// start test runner
+foreach ($handlers as $handlerName) {
+    // check extension
+    if (in_array($handlerName, $extensions) && !extension_loaded($handlerName)) {
+        if (($argv[1] ?? '') != 'silent') {
+            echo sprintf("%-10s: SKIPPED\n", $handlerName);
+        }
+        continue;
+    }
+    // start servers
+    $serverPids = [];
+    for ($j = 0; $j < $parallel; $j++) {
+        $port = 9000 + $j;
+        $serverPids[] = trim(exec("php -S localhost:$port > tmp/$port.server.log 2>&1 & echo \$!"));
+    }
+    // wait for ports
+    for ($j = 0; $j < $parallel; $j++) {
+        $port = 9000 + $j;
+        while (false === ($fp = @fsockopen('localhost', $port, $errCode, $errStr))) {
+            usleep(10 * 1000);
+        }
+        fclose($fp);
+    }
+    // execute scenarios
+    $testsFailed = 0;
+    $testsExecuted = 0;
+    foreach (glob("tests/*.log") as $testFile) {
+        $content = file_get_contents($testFile);
+        list($head, $body) = explode("\n===\n", $content, 2);
+        $paths = [];
+        foreach (explode("\n", trim($head)) as $line) {
+            list($count, $path) = explode(' ', $line);
+            $paths[] = [$path, $count];
+        }
+        $sessionName = '';
+        $sessionId = '';
+        $responses = [];
+        // execute requests
+        foreach ($paths as list($path, $count)) {
+            $clientPids = [];
+            for ($j = 0; $j < $count; $j++) {
+                $port = 9000 + $j;
+                $clientPids[] = trim(exec("curl -i -sS -b '$sessionName=$sessionId' http://localhost:$port/run-tests.php?$handlerName/$path -o tmp/$port.client.log & echo \$!"));
+            }
+            exec("wait " . implode(' ', $clientPids));
+            flush();
+            $results = [];
+            for ($j = 0; $j < $count; $j++) {
+                $port = 9000 + $j;
+                // parse response into headers and body
+                list($header, $logFile) = explode("\r\n\r\n", trim(file_get_contents("tmp/$port.client.log")), 2);
+                $headerLines = explode("\r\n", $header);
+                $headers = [];
+                array_shift($headerLines);
+                foreach ($headerLines as $headerLine) {
+                    list($key, $value) = explode(': ', $headerLine);
+                    $headers[$key] = $value;
+                }
+                // detect session name and session id
+                if (isset($headers['Set-Cookie'])) {
+                    $oldSessionId = $sessionId;
+                    list($sessionName, $sessionId) = explode('=', explode(';', $headers['Set-Cookie'])[0]);
+                }
+                // replace random session ids
+                $replacements = [
+                    $sessionId => '{{current_random_session_id}}',
+                    $oldSessionId => '{{previous_random_session_id}}',
+                ];
+                if (isset($headers['X-Session-Save-Path'])) {
+                    $replacements[$headers['X-Session-Save-Path']] = '{{session_save_path}}';
+                }
+                $resultLogFile = str_replace(array_keys($replacements), array_values($replacements), $logFile);
+                // store with flush time as key (if available)
+                if (isset($headers['X-Session-Flush-At'])) {
+                    $timestamp = $headers['X-Session-Flush-At'];
+                    $results[$timestamp] = $resultLogFile;
+                } else {
+                    $results[] = $resultLogFile;
+                }
+            }
+            ksort($results);
+            $responses = array_merge($responses, $results);
+        }
+        // compare and report
+        $newbody = implode("\n---\n", $responses);
+        if (trim($body)) {
+            if ($body != $newbody) {
+                echo "$testFile.$handlerName.out - FAILED\n";
+                file_put_contents("$testFile.$handlerName.out", "$head\n===\n$newbody");
+                $testsFailed += 1;
+            }
+        } else {
+            file_put_contents($testFile, "$head\n===\n$newbody");
+        }
+        $testsExecuted += 1;
+    }
+    // stop servers
+    foreach ($serverPids as $serverPid) {
+        exec("kill $serverPid");
+    }
+    if (($argv[1] ?? '') != 'silent') {
+        echo sprintf("%-10s: %s\n", $handlerName, $testsFailed ? 'FAILED' : 'OK');
+    }
+}
---
+++ src/LoggingSessionHandler.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace MintyPHP;
+
+use SessionHandlerInterface;
+use SessionIdInterface;
+use SessionUpdateTimestampHandlerInterface;
+
+class LoggingSessionHandler implements SessionHandlerInterface, SessionIdInterface, SessionUpdateTimestampHandlerInterface
+{
+    private $sessionHandler;
+
+    public function __construct($sessionHandler)
+    {
+        $this->sessionHandler = $sessionHandler;
+    }
+
+    public function __call($name, $arguments)
+    {
+        $parameters = json_encode($arguments, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        if (method_exists($this->sessionHandler, $name)) {
+            $result = $this->sessionHandler->$name(...$arguments);
+        } else {
+            $className = get_class($this->sessionHandler);
+            switch ($name) {
+                case 'create_sid': // from SessionIdInterface
+                    $result = bin2hex(random_bytes(16));
+                    break;
+                case 'updateTimestamp': // from SessionUpdateTimestampHandlerInterface
+                    $result = $this->sessionHandler->write(...$arguments);
+                    break;
+                default:
+                    throw new \Exception("The class '$className' is missing method '$name'");
+            }
+        }
+        $return = json_encode($result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        echo "$name $parameters = $return\n";
+        return $result;
+    }
+
+    public function close(): bool
+    {
+        return $this->__call('close', func_get_args());
+    }
+
+    public function destroy($id): bool
+    {
+        return $this->__call('destroy', func_get_args());
+    }
+
+    public function gc($maxlifetime): bool
+    {
+        return $this->__call('gc', func_get_args());
+    }
+
+    public function open($save_path, $session_name): bool
+    {
+        return $this->__call('open', func_get_args());
+    }
+
+    public function read($id): string
+    {
+        return $this->__call('read', func_get_args());
+    }
+
+    public function write($id, $session_data): bool
+    {
+        return $this->__call('write', func_get_args());
+    }
+
+    public function create_sid(): string
+    {
+        return $this->__call('create_sid', func_get_args());
+    }
+
+    public function validateId($id): bool
+    {
+        return $this->__call('validateId', func_get_args());
+    }
+
+    public function updateTimestamp($id, $session_data): bool
+    {
+        return $this->__call('updateTimestamp', func_get_args());
+    }
+}
